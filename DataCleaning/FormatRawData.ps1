368e4470ab6f666b80a3110a601184c6de701581
Stella Knief <s.knief@uqconnect.edu.au>
2023-05-26 12:17:17 +1000

    WOOOOOO IT WORKED

--- DataCleaning/FormatRawData.ps1
+++ DataCleaning/FormatRawData.ps1
@@ -1,11 +1,6 @@
 # Specify the root folder containing the txt files and subfolders
 $rootFolderPath = "C:\Users\sknie\Desktop\pipetest"
 
-## Step 1 goes here
-# Get the list of txt files in the root folder and subfolders
-#$txtFiles = Get-ChildItem -Path $rootFolderPath -Filter "*.txt" -Recurse
-
-
 ## Step 2: Change Headers 
 $txtFiles = Get-ChildItem -Path $rootFolderPath -Filter "*_cleaned.txt" -Recurse
 
@@ -16,10 +11,8 @@ foreach ($txtFile in $txtFiles) {
     # Identify the header row and split it into column headers
     $originalHeaders = $content[0] -split '\s+'
 
-    #Step 2.1: replace headers with index values (because they are different across channel files)
-
-            # Modify the column headers
-        $modifiedHeaders = for ($i = 0; $i -lt $originalHeaders.Count; $i++) {
+    # Step 2.1: replace headers with index values (because they are different across channel files)
+    $modifiedHeaders = for ($i = 0; $i -lt $originalHeaders.Count; $i++) {
         if ($i -lt 18) {
             "Variable$($i + 1)"
         }
@@ -27,11 +20,11 @@ foreach ($txtFile in $txtFiles) {
             # Stop the loop after 18 index increases
             break
         }
-}
-        # Replace the original header row with the modified column headers
-        $content[0] = $modifiedHeaders[0..17] -join ' '
-
-        # Write the updated content back to the text file
-        $content | Set-Content -Path $txtFile.FullName
-    
     }
+
+    # Replace the original header row with the modified column headers
+    $content[0] = $modifiedHeaders -join "`t"
+
+    # Write the updated content back to the text file
+    $content | Set-Content -Path $txtFile.FullName
+}
--- DataCleaning/Step2.ps1
+++ DataCleaning/Step2.ps1
@@ -1,3 +1,26 @@
+  # Identify the header row and split it into column headers
+  $originalHeaders = $content[0] -split '\s+'
+
+  #Step 2.1: replace headers with index values (because they are different across channel files)
+
+          # Modify the column headers
+      $modifiedHeaders = for ($i = 0; $i -lt $originalHeaders.Count; $i++) {
+      if ($i -lt 18) {
+          "Variable$($i + 1)"
+      }
+      else {
+          # Stop the loop after 18 index increases
+          break
+      }
+}
+      # Replace the original header row with the modified column headers
+      $originalHeaders = $content[0] -split 
+
+      # Write the updated content back to the text file
+      $content | Set-Content -Path $txtFile.FullName
+  
+
+
 
   #Step 2.2: then redo these with names that matter
 
